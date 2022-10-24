89cfbe2b4a81389ba26470c23ff2ef2c762f0684
ignasi <ignasi.lopez.luna@gmail.com>
2022-10-24 10:50:04 +0200

    Added testcontainers test

--- src/TestcontainersCloud.test.js
+++ src/TestcontainersCloud.test.js
@@ -1,3 +1,30 @@
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
+const redis = require("async-redis");
+const { GenericContainer } = require("testcontainers");
+
+describe("GenericContainer", () => {
+    let container;
+    let redisClient;
+
+    beforeAll(async () => {
+        jest.setTimeout(10000);
+        container = await new GenericContainer("redis")
+            .withExposedPorts(6379)
+            .start();
+
+        redisClient = redis.createClient(
+            container.getMappedPort(6379),
+            container.getHost(),
+        );
+    });
+
+    afterAll(async () => {
+        await redisClient.quit();
+        await container.stop();
+    });
+
+    it("works", async () => {
+        await redisClient.set("key", "val");
+        expect(await redisClient.get("key")).toBe("val");
+        expect(await redisClient.get("other-key")).toBeNull();
+    });
 });
